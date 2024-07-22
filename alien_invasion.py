5bb362ecec9da85d51be303cda7c00abae2c6436
Mateusz Grot <mateuszgrot@wp.pl>
2024-07-22 14:50:27 +0200

    Just started

---
+++ alien_invasion.py
@@ -0,0 +1,24 @@
+import  sys
+import pygame
+
+class AlienInvasion:
+    '''Klasa przeznaczona do zarządzania zasobami i sposobem działania gry'''
+
+    def __init__(self):
+        '''Inicjalizacja gry i jej zasobów'''
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Inwazja obcych")
+
+    def run_game(self):
+        '''Rozpoczęcie pętli głównej gry.'''
+        while True:
+            #Oczekiwanie na naciśnięcie klawisza lub przycisku myszy.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                pygame.display.flip()
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
