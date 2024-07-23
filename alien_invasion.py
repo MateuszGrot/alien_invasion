f3db41861ea4edd528e3cd5b3f5bc42164c8efd7
Mateusz Grot <mateuszgrot@wp.pl>
2024-07-23 14:40:18 +0200

    Setup a new bg-color

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
-import  sys
+import sys
 import pygame
+
 
 class AlienInvasion:
     '''Klasa przeznaczona do zarządzania zasobami i sposobem działania gry'''
@@ -8,18 +9,21 @@ class AlienInvasion:
         '''Inicjalizacja gry i jej zasobów'''
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Inwazja obcych")
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         '''Rozpoczęcie pętli głównej gry.'''
         while True:
-            #Oczekiwanie na naciśnięcie klawisza lub przycisku myszy.
+            # Oczekiwanie na naciśnięcie klawisza lub przycisku myszy.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
                 pygame.display.flip()
                 self.clock.tick(60)
+            self.screen.fill(self.bg_color)
+
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
